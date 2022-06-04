2ecd9eb0c856122c4a73713c745fed6ac2e9065e
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2022-06-04 15:36:16 +0700

    🐛 fix: update sql file

--- db_pawang.sql
+++ db_pawang.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 24, 2022 at 04:58 AM
--- Server version: 10.7.3-MariaDB
--- PHP Version: 8.1.6
+-- Host: mysql_db:3306
+-- Generation Time: Jun 04, 2022 at 08:35 AM
+-- Server version: 5.7.38
+-- PHP Version: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,11 +44,11 @@ CREATE TABLE `categories` (
 
 CREATE TABLE `transactions` (
   `id` int(10) UNSIGNED NOT NULL,
-  `amount` double NOT NULL,
+  `amount` bigint(20) NOT NULL,
   `category_id` int(10) UNSIGNED NOT NULL,
   `wallet_id` int(10) UNSIGNED NOT NULL,
   `type` enum('income','outcome') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
   `image_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `date` datetime NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
@@ -83,7 +83,7 @@ CREATE TABLE `wallets` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
-  `balance` double NOT NULL,
+  `balance` bigint(20) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
