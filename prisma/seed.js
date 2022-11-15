0e35e3f5a68e191952c50cfea33a2e61b35f4cbd
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2022-11-15 22:48:54 +0700

    add: seeder for app_versioning

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,110 +1,126 @@
-const { PrismaClient } = require('@prisma/client');
-const e = require('express');
+const { PrismaClient } = require("@prisma/client");
+const e = require("express");
 const prisma = new PrismaClient();
 
 const categoryData = [
-    {
-        name: 'Belanja',
-        icon: '/public/assets/categories/belanja.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Makan & Minum',
-        icon: '/public/assets/categories/makan-minum.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Transportasi',
-        icon: '/public/assets/categories/transportasi.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Pakaian',
-        icon: '/public/assets/categories/pakaian.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Pakaian',
-        icon: '/public/assets/categories/pakaian.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Pendidikan',
-        icon: '/public/assets/categories/pendidikan.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Hiburan',
-        icon: '/public/assets/categories/hiburan.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Elektronik',
-        icon: '/public/assets/categories/elektronik.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Kesehatan',
-        icon: '/public/assets/categories/kesehatan.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Asuransi',
-        icon: '/public/assets/categories/asuransi.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Pengeluaran Lainnya',
-        icon: '/public/assets/categories/pengeluaran-lainnya.png',
-        type: 'outcome',
-    },
-    {
-        name: 'Gaji',
-        icon: '/public/assets/categories/gaji.png',
-        type: 'income',
-    },
-    {
-        name: 'Orang Tua',
-        icon: '/public/assets/categories/orang-tua.png',
-        type: 'income',
-    },
-    {
-        name: 'Pemasukkan Lainnya',
-        icon: '/public/assets/categories/pemasukan-lainnya.png',
-        type: 'income',
-    },
+  {
+    name: "Belanja",
+    icon: "/public/assets/categories/belanja.png",
+    type: "outcome",
+  },
+  {
+    name: "Makan & Minum",
+    icon: "/public/assets/categories/makan-minum.png",
+    type: "outcome",
+  },
+  {
+    name: "Transportasi",
+    icon: "/public/assets/categories/transportasi.png",
+    type: "outcome",
+  },
+  {
+    name: "Pakaian",
+    icon: "/public/assets/categories/pakaian.png",
+    type: "outcome",
+  },
+  {
+    name: "Pakaian",
+    icon: "/public/assets/categories/pakaian.png",
+    type: "outcome",
+  },
+  {
+    name: "Pendidikan",
+    icon: "/public/assets/categories/pendidikan.png",
+    type: "outcome",
+  },
+  {
+    name: "Hiburan",
+    icon: "/public/assets/categories/hiburan.png",
+    type: "outcome",
+  },
+  {
+    name: "Elektronik",
+    icon: "/public/assets/categories/elektronik.png",
+    type: "outcome",
+  },
+  {
+    name: "Kesehatan",
+    icon: "/public/assets/categories/kesehatan.png",
+    type: "outcome",
+  },
+  {
+    name: "Asuransi",
+    icon: "/public/assets/categories/asuransi.png",
+    type: "outcome",
+  },
+  {
+    name: "Pengeluaran Lainnya",
+    icon: "/public/assets/categories/pengeluaran-lainnya.png",
+    type: "outcome",
+  },
+  {
+    name: "Gaji",
+    icon: "/public/assets/categories/gaji.png",
+    type: "income",
+  },
+  {
+    name: "Orang Tua",
+    icon: "/public/assets/categories/orang-tua.png",
+    type: "income",
+  },
+  {
+    name: "Pemasukkan Lainnya",
+    icon: "/public/assets/categories/pemasukan-lainnya.png",
+    type: "income",
+  },
 ];
 
-const main = async () => {
-    console.log(`🔥 Start seeding ...`);
-    for (const i of categoryData) {
-        const category = await prisma.categories.upsert({
-            where: {
-                name: i.name
-            },
-            create: {
+const app_version = {
+  version: "4.1.0",
+  force_update: false,
+};
 
-                name: i.name,
-                icon: i.icon,
-                type: i.type,
-            },
-            update: {
-                name: i.name,
-                icon: i.icon,
-                type: i.type,
-            }
-        })
-        console.log(`Success create category ${category.name}`);
-    }
-    console.log(`🚀 Seeding finished ... `);
-}
+const main = async () => {
+  console.log(`🔥 Start seeding ...`);
+  for (const i of categoryData) {
+    const category = await prisma.categories.upsert({
+      where: {
+        name: i.name,
+      },
+      create: {
+        name: i.name,
+        icon: i.icon,
+        type: i.type,
+      },
+      update: {
+        name: i.name,
+        icon: i.icon,
+        type: i.type,
+      },
+    });
+    console.log(`Success create category ${category.name}`);
+  }
+
+  const checkVersioning = await prisma.app_versioning.findFirst();
+  if (!checkVersioning) {
+    const versioning = await prisma.app_versioning.create({
+      data: {
+        version: app_version.version,
+        force_update: app_version.force_update,
+      },
+    });
+    console.log(`Success create app versioning ${versioning.version}`);
+  }
+
+  console.log(`🚀 Seeding finished ... `);
+};
 
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect();
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
