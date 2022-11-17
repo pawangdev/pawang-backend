670d40bbd3f80aca332087e2796653e92b8a7eca
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2022-11-17 23:44:03 +0700

    fix: add try catch on middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,29 +3,38 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) throw { status: 401, message: "Unauthorized", data: null };
+    if (token == null)
+      throw { status: 401, message: "Unauthorized", data: null };
 
-  jwt.verify(token, process.env.TOKEN_SECRET_KEY, async (err, user) => {
-    try {
-      if (err) throw { status: 401, message: "Unauthorized", data: null };
-      const auth = await prisma.users.findUnique({
-        where: {
-          id: user.id,
-        },
-      });
+    jwt.verify(token, process.env.TOKEN_SECRET_KEY, async (err, user) => {
+      try {
+        if (err) throw { status: 401, message: "Unauthorized", data: null };
+        const auth = await prisma.users.findUnique({
+          where: {
+            id: user.id,
+          },
+        });
 
-      if (!auth) throw { status: 401, message: "Unauthorized", data: null };
-      req.user = auth;
-      next();
-    } catch (error) {
-      return res.status(error.status || 500).json({
-        status: false,
-        message: error.message || "INTERNAL_SERVER_ERROR",
-        data: null,
-      });
-    }
-  });
+        if (!auth) throw { status: 401, message: "Unauthorized", data: null };
+        req.user = auth;
+        next();
+      } catch (error) {
+        return res.status(error.status || 500).json({
+          status: false,
+          message: error.message || "INTERNAL_SERVER_ERROR",
+          data: null,
+        });
+      }
+    });
+  } catch (error) {
+    return res.status(error.status || 500).json({
+      status: false,
+      message: error.message || "INTERNAL_SERVER_ERROR",
+      data: null,
+    });
+  }
 };
